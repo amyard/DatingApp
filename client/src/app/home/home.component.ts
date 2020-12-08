4a7abf4bb7f01f05450f1b4e33cc8c20445d0288
amyard <maksymturenko@gmail.com>
2020-12-09 01:38:02 +0300

    client - register.combine register services.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,26 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   registerMode = false;
-  users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToogle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    });
   }
 
   cancelRegisterMode(event: boolean) {
