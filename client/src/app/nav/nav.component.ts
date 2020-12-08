40ce755c7b788f63c6b510cafd045ac3b2c79dc8
amyard <maksymturenko@gmail.com>
2020-12-09 00:32:23 +0300

    api - save login token in storage. clean code.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,18 +10,14 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean;
+  // loggedIn: boolean;  
+  constructor(public accountService: AccountService) { }
 
-  constructor(private accountService: AccountService) { }
-
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     });
@@ -27,15 +25,5 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;  // convert object to boolean
-    }, error => {
-      console.log(error);
-    });
-  }
-
 }
