05d57799fbdc62110fed7e56b404f16db07edbe4
Maksym Turenko <maksym.turenko@geeksforless.com>
2020-12-12 15:05:39 +0200

    api - create error middleware

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(int statusCoce, string message = null, string details = null)
+        {
+            StatusCoce = statusCoce;
+            Message = message;
+            Details = details;
+        }
+
+        public int StatusCoce { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
