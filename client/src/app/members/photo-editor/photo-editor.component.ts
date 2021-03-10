e045901f0e919ad660309f2368a956e0423f8124
amyard <maksymturenko@gmail.com>
2021-03-11 03:56:47 +0900

    client - add file uploaded on UI

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Member} from "../../models/member";
+import {FileUploader} from "ng2-file-upload";
+import {environment} from "../../../environments/environment";
+import {AccountService} from "../../_services/account.service";
+import {User} from "../../models/user";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,10 +14,46 @@ import {Member} from "../../models/member";
 export class PhotoEditorComponent implements OnInit {
   /* @Input() member: Member; - will receive correct member from <app-photo-editor [member]="member"></app-photo-editor>  */
   @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
 
   ngOnInit(): void {
+    this.initializeUpload();
+  }
+
+  // from documentation https://valor-software.com/ng2-file-upload/
+
+  fileOverBase(e: any) {
+    this.hasBaseDropzoneOver = e;
+  }
+
+  initializeUpload() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if(response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
   }
 
 }
