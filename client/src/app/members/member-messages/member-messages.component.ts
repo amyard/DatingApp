de7723868b31aafb4690ee2f585bb6504014eb04
amyard <maksymturenko@gmail.com>
2021-04-09 06:30:11 +0900

    client - send message from MessageForm

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,16 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from "../../models/message";
 import {MessageService} from "../../_services/message.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
 })
+// @ViewChild('messageForm') messageForm: NgForm; - add access to form
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm: NgForm;
   @Input() messages: Message[];
+  @Input() username: string;
+  messageContent: string;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {}
+
+  sendMessage() {
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message => {
+      this.messages.push(message);
+      this.messageForm.reset();
+    })
+  }
 }
