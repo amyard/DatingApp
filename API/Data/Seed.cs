9c45ae7eef5e4ee6585aba4bf0084de23160a355
amyard <maksymturenko@gmail.com>
2021-04-11 04:50:19 +0900

    API - roles to new users. create admin user.

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,7 +9,7 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
@@ -18,11 +18,33 @@ namespace API.Data
             
             if (users == null) return;
 
+            var roles = new List<AppRole>
+            {
+                new AppRole {Name = "Member"},
+                new AppRole {Name = "Admin"},
+                new AppRole {Name = "Moderator"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach(var user in users)
             {
                 user.UserName = user.UserName.ToLower();
                 await userManager.CreateAsync(user, "Admin123");
+
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Admin123");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
         }
     }
 }
